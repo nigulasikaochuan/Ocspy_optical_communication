2ea5b0c918487e63f4e4839d901d755ea828039c
huazhilun <nigulasikaochuan@sjtu.edu.cn>
2019-06-25 18:25:30 +0800

    xixi

--- qamdata/__init__.py
+++ qamdata/__init__.py
@@ -1,15 +1,18 @@
-from ..ReceiverDsp.dsp_tools import cal_symbols_qam
-from ..ReceiverDsp.dsp_tools import cal_scaling_factor_qam
-
+# from ..ReceiverDsp.dsp_tools import cal_symbols_qam
+# from ..ReceiverDsp.dsp_tools import cal_scaling_factor_qam
+#
 
 # # import numpy as np
 # # QPSK = cal_symbols_qam(4)/np.sqrt(cal_scaling_factor_qam((4)))
 # #
 # #
 
+
+
 import os
-names = os.listdir('./')
-if names:
+names = os.listdir('./*.mat')
+if not names:
+    names = os.listdir('./')
     for name in names:
         if 'qam' in name and name.endswith('py'):
 
